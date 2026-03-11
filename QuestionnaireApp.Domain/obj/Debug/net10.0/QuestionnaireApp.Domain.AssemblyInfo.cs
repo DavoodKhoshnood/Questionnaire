a02df7743332cad30af3d35ada6aaa76fa4ab6c8
DavoodKhoshnood <khoshnood1977@gmail.com>
2026-03-11 01:24:32 +0000

    Fixed merge conflicts

--- QuestionnaireApp.Domain/obj/Debug/net10.0/QuestionnaireApp.Domain.AssemblyInfo.cs
+++ QuestionnaireApp.Domain/obj/Debug/net10.0/QuestionnaireApp.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuestionnaireApp.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06273867208c52698e06c38059f02c48425206f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuestionnaireApp.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuestionnaireApp.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
